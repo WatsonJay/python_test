849baeb0b1336c080cc89be8ef9dad8a5bdb6633
WatsonJay <446725026@qq.com>
2019-04-29 23:43:24 +0800

    update

--- hello_world/pyqt_file_download/main/bugger.py
+++ hello_world/pyqt_file_download/main/bugger.py
@@ -6,28 +6,70 @@ from lxml import etree
 
 # 取消代理验证
 ssl._create_default_https_context = ssl._create_unverified_context
+#引入url头格式
+headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36'
+    }
+#参数化url
+url_search = "/search/index.asp?page={}&searchword={}&searchtype=-1"
+url_head = "https://www.meijutt.com"
 
 def searchingflim(name,page):
     # 将文本转为 gb2312 编码格式
     name = parse.quote(name.encode('gb2312'))
-    #引入url头格式
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36'
-    }
-    #参数化url
-    url = "https://www.meijutt.com/search/index.asp?page={}&searchword={}&searchtype=-1"
     #请求页面
     try:
-        reponse = requests.get(url, headers=headers)
+        reponse = requests.get(url_head+url_search.format(page,name), headers=headers)
+        reponse.encoding = 'gb2312'
+        selector = etree.HTML(reponse.text)
+        pageinfo = selector.xpath('//div[@class="page"][1]/span/text()')
+        if pageinfo:
+            if page=="":
+                pages = pageinfo[0].split('/')[1].rstrip("页")
+                return pages
+            else:
+                filmList = listfilm(selector)
+                return filmList
+        else:
+            return "no such film"
         if reponse.status_code == 200:
             return reponse.text
         else:
-            return None
+            return "connect false"
     except requests.RequestException:
-        return None
+        return "connect false"
+
+def listfilm(selector):
+    # 获取每一季的内容（剧名和链接）
+    node_list = selector.xpath('//a[@class="B font_14"]')
+    filmList = []
+    for node in node_list:
+        # 获取信息
+        title = node.xpath('@title')[0]
+        link = node.xpath('@href')[0]
+        film_downpath(link, filmList,title)
+    return filmList
+
+def film_downpath(link,filmList,title):
+    reponse1 = requests.get(url_head + link, headers=headers)
+    reponse1.encoding = 'gb2312'
+    downPath = etree.HTML(reponse1.text)
+    paths = downPath.xpath('//div[@class="tabs-list current-tab"]//li/p')
+    index = 1
+    for path in paths:
+        items = {}
+        items["title"] = title
+        if len(path.xpath('./em/text()'))==0:
+            items["size"] = "未知大小"
+        else:
+            items["size"] = path.xpath('./em/text()')[0].strip("[]")
+        items["jishu"] = index
+        items["link"] = path.xpath('./strong//@href')[0]
+        index=index+1
+        filmList.append(items)
 
 
 if __name__ == '__main__':
     #check()
     #proxyip()
-    searchingflim("绿箭侠")
+    searchingflim("绿箭侠","1")
--- hello_world/pyqt_file_download/main/film_downloader.py
+++ hello_world/pyqt_file_download/main/film_downloader.py
@@ -1,5 +1,6 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, qApp,QMessageBox
+from hello_world.pyqt_file_download.main.bugger import searchingflim
 from hello_world.pyqt_file_download.main.gui import Ui_MainWindow
 
 
@@ -14,6 +15,59 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
             self, '提示', '是否开始爬取《' + self.lineEdit.text() + "》",
                         QMessageBox.Ok | QMessageBox.No, QMessageBox.Ok
         )
+        if start == QMessageBox.Ok:
+            self.page = 1
+            pages = searchingflim(self.lineEdit.text(),"")
+            if pages == "no such film":
+                self.infoSearchNull()
+            elif pages == "connect false":
+                self.infoSearchError()
+            else:
+                for page in range(int(pages)):
+                    filmList = searchingflim(self.lineEdit.text(), page+1)
+                    self.writeDetailPage(filmList)
+                self.infoSearchDone()
+        # 取消爬取
+        else:
+            pass
+
+            # 将数据显示到图形界面
+
+    def writeDetailPage(self, filmList):
+        for film in filmList:
+            # 写入图形界面
+            self.textEdit.append(
+                "<div>"
+                "<font color='red' size='3'>" + film["title"] + "</font>" + "\n"
+                "<font color='orange' size='3'>第" + film["jishu"] + "集</font>" + "\n"
+                "<font color='green' size='3'>下载链接：</font>" + "\n"
+                "<font color='blue' size='3'>" +film["link"] + "</font>"+ "\n"
+                "<font color='green' size='3'>下载大小：" +film["size"]+ "</font>"                                               
+                "<p></p>"
+                "</div>"
+            )
+
+    # 搜索不到结果的提示信息
+    def infoSearchNull(self):
+         QMessageBox.information(
+             self, '提示', '搜索结果不存在，请重新输入搜索内容',
+             QMessageBox.Ok, QMessageBox.Ok
+         )
+
+    # 爬取数据完毕的提示信息
+    def infoSearchDone(self):
+        QMessageBox.information(
+            self, '提示', '爬取《' + self.lineEdit.text() + '》完毕',
+            QMessageBox.Ok, QMessageBox.Ok
+        )
+
+    # 多页情况下是否继续爬取的提示信息
+    def infoSearchError(self):
+        QMessageBox.information(
+            self, '提示', '爬取异常',
+        QMessageBox.Ok, QMessageBox.Ok
+                )
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
