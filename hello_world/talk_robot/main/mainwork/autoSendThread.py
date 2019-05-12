1d4b4eab3a6056ee406c95ae368e0e361533dbfe
WatsonJay <446725026@qq.com>
2019-05-12 20:39:55 +0800

    添加按设置中的群名进行发送

--- hello_world/talk_robot/main/mainwork/autoSendThread.py
+++ hello_world/talk_robot/main/mainwork/autoSendThread.py
@@ -3,6 +3,7 @@ import os
 from time import sleep
 
 from PyQt5.QtCore import QThread,pyqtSignal
+from hello_world.talk_robot.main.config.config import config
 import itchat
 
 class autoSend(QThread):
@@ -25,9 +26,16 @@ class autoSend(QThread):
         # itchat.run()
 
         try:
-            for i in range(200000):
-                itchat.send(msg= self.message, toUserName='filehelper')
-                self.getMsgSignal.emit(self.message + "发送成功,间隔"+ str(self.sleepTime) + "s")
+            config_get =config()
+            groups_selects= config_get.splitword(config_get.getOption('qun','name'))
+            groups = itchat.get_chatrooms(update=True)
+            while True:
+                i = 0
+                for group in groups:
+                    if group['NickName'] in groups_selects:
+                        itchat.send_msg("{}".format(self.message), group['UserName'])
+                        i = i+1
+                self.getMsgSignal.emit(self.message + "发送成功,合计发送"+str(len(groups_selects))+"个群,成功"+str(i)+"个,间隔"+ str(self.sleepTime) + "s")
                 sleep(self.sleepTime)
         except Exception as e:
             pass
