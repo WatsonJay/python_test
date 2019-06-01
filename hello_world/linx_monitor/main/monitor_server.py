f36232bf1617cc0ce1c8de58368196605c2583d9
WatsonJay <446725026@qq.com>
2019-06-01 22:36:10 +0800

    Update monitor_server.py

--- hello_world/linx_monitor/main/monitor_server.py
+++ hello_world/linx_monitor/main/monitor_server.py
@@ -2,6 +2,7 @@
 # @Author  : Jaywatson
 # @File    : monitor_server.py
 # @Software: PyCharm
+import re
 
 import paramiko
 
@@ -45,16 +46,47 @@ class Monitor_server:
 
     '''内存监控'''
 
-    def mem_info(self, linuxInfo):
+    def get_mem_info(self, sshClient):
         command = 'cat /proc/meminfo'
+        get_msgs = self.sshExecCmd(sshClient, command)
+        MemTotal = int(re.findall("MemTotal\:\s*(\d+)\skB\n", "".join(get_msgs))[0])
+        MemFree = int(re.findall("MemFree\:\s*(\d+)\skB\n", "".join(get_msgs))[0])
+        UsedMem = round((MemTotal - MemFree) / MemTotal * 100, 2)
+        Buffers = round(int(re.findall("Buffers\:\s*(\d+)\skB\n", "".join(get_msgs))[0]) / MemTotal * 100, 2)
+        Cached = round(int(re.findall("Cached\:\s*(\d+)\skB\n", "".join(get_msgs))[0]) / MemTotal * 100, 2)
+        SwapCached = round(int(re.findall("SwapCached\:\s*(\d+)\skB\n", "".join(get_msgs))[0]) / MemTotal * 100, 2)
+        Mem_info = [UsedMem, Buffers, Cached, SwapCached]
+        return Mem_info
 
     '''磁盘空间监控'''
 
-    def disk_stat(self, linuxInfo):
+    def get_disk_stat(self, sshClient):
         command = 'df -h'
 
     '''端口监控'''
 
-    def get_Com_Str(self, linuxInfo):
+    def get_Com_Str(self, sshClient):
         command = 'netstat -tpln'
 
+    def get_Cpu_info(self, sshClient):
+        command = 'top -b -n 1'
+        get_msgs = self.sshExecCmd(sshClient, command)
+        user_used = float(re.findall("(\d+\.?\d*)\sus\,", "".join(get_msgs))[0])
+        sys_used = float(re.findall("(\d+\.?\d*)\ssy\,", "".join(get_msgs))[0])
+        free = float(re.findall("(\d+\.?\d*)\sid\,", "".join(get_msgs))[0])
+        total_Used = round(100.0 - free, 2)
+        cpu_info = [user_used, sys_used, total_Used]
+        return cpu_info
+
+
+if __name__ == '__main__':
+    hostname = '192.168.132.129'
+    username = 'root'
+    password = 'jaywatson'
+    port = 22
+    moni = Monitor_server()
+    test_sshConnect = moni.sshConnect(hostname,port,username,password)
+    cpu_info = moni.get_Cpu_info(test_sshConnect)
+    Mem_info = moni.get_mem_info(test_sshConnect)
+    print(cpu_info)
+    print(Mem_info)
