9981a7ccf6e00bfd99b799b83620ff3d24e7bdac
WatsonJay <446725026@qq.com>
2019-06-01 22:36:43 +0800

    Update linux_remoteControl.py

--- hello_world/linux_remoteControl.py
+++ hello_world/linux_remoteControl.py
@@ -1,17 +1,22 @@
 import paramiko
 import re
 
-hostname = '23.105.214.192'
+hostname = '192.168.132.129'
 username = 'root'
-password = 'rTZ6nzQKmFfQ'
-port = 28676
+password = 'jaywatson'
+port = 22
 
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh.connect(hostname=hostname,port=port,username=username,password=password)
-stdin, stdout, stderr = ssh.exec_command("top -H -b -d 1 -n 1 ")
+stdin, stdout, stderr = ssh.exec_command("cat /proc/meminfo")
 filesystem_usage = stdout.readlines()
-res = "".join(filesystem_usage)
-mem_values = re.findall("", res)
-print(filesystem_usage)
+MemTotal = int(re.findall("MemTotal\:\s*(\d+)\skB\n","".join(filesystem_usage))[0])
+MemFree = int(re.findall("MemFree\:\s*(\d+)\skB\n","".join(filesystem_usage))[0])
+UsedMem = round((MemTotal - MemFree) / MemTotal * 100, 2)
+Buffers = round(int(re.findall("Buffers\:\s*(\d+)\skB\n","".join(filesystem_usage))[0])/ MemTotal * 100, 2)
+Cached = round(int(re.findall("Cached\:\s*(\d+)\skB\n","".join(filesystem_usage))[0])/ MemTotal * 100, 2)
+SwapCached = round(int(re.findall("SwapCached\:\s*(\d+)\skB\n","".join(filesystem_usage))[0])/ MemTotal * 100, 2)
+Mem_info = [UsedMem, Buffers, Cached, SwapCached]
+print(Mem_info)
 ssh.close()
