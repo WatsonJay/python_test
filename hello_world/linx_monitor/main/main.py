af524467a080b22d27478b19eec4f3350027482f
WatsonJay <446725026@qq.com>
2019-06-08 22:58:51 +0800

    Create main.py

---
+++ hello_world/linx_monitor/main/main.py
@@ -0,0 +1,85 @@
+import sys
+
+from PyQt5.QtWidgets import QApplication, QMainWindow, QDialog, QMessageBox
+
+from hello_world.linx_monitor.main.config.config import config
+from hello_world.linx_monitor.main.ui.Config_Dialog import Ui_Config_Dialog
+from hello_world.linx_monitor.main.ui.MonitorWindow import Ui_Monitor_Window
+
+
+class MyMainWindow(QMainWindow, Ui_Monitor_Window):
+    def __init__(self, parent=None):
+        super(MyMainWindow, self).__init__(parent)
+        self.setupUi(self)
+        self.loadConfig()
+        self.serverAdd.clicked.connect(self.serverAdd_Open)
+        self.server_listWidget.itemDoubleClicked.connect(self.showWidget)
+
+    def loadConfig(self):
+        try:
+            conf = config()
+            SentionList = conf.getSection()
+            self.server_listWidget.clear()
+            self.server_listWidget.addItems(SentionList)
+        except Exception as e:
+            self.Tips("配置文件出现异常，请重置配置文件")
+
+    def serverAdd_Open(self):
+        conf = config()
+        self.serverConfigDialog = serverConfigDialog()
+        if self.serverConfigDialog.exec_():
+            infos = self.serverConfigDialog.getDict()
+            conf.cmdSection(infos['name'],infos)
+        self.serverConfigDialog.destroy()
+        self.loadConfig()
+
+    def showWidget(self):
+        conf = config()
+        self.serverConfigDialog = serverConfigDialog()
+        sention = self.server_listWidget.item(self.server_listWidget.currentRow()).text()
+        info = conf.sentionAll(sention)
+        self.serverConfigDialog.setDict(info)
+        self.serverConfigDialog.disable()
+        if self.serverConfigDialog.exec_():
+            self.Tips("")
+        self.serverConfigDialog.destroy()
+
+
+    def Tips(self, message):
+        QMessageBox.about(self, "提示", message)
+
+class serverConfigDialog(QDialog, Ui_Config_Dialog):
+    def __init__(self, parent=None):
+        super(serverConfigDialog, self).__init__(parent)
+        self.setupUi(self)
+
+    def setDict(self, dict):
+        conf = config()
+        self.nameEdit.setText(dict['name'])
+        self.ipEdit.setText(dict['ip'])
+        self.portEdit.setText(dict['port'])
+        self.userEdit.setText(dict['user'])
+        self.passwordEdit.setText(conf.decrypt(dict['userpassword']))
+
+    def getDict(self):
+        conf = config()
+        info ={}
+        info['name'] = self.nameEdit.text()
+        info['ip'] = self.ipEdit.text()
+        info['port'] = self.portEdit.text()
+        info['user'] = self.userEdit.text()
+        info['password'] = conf.encrypt(self.nameEdit.text())
+        return info
+
+    def disable(self):
+        self.nameEdit.setDisabled(True)
+        self.ipEdit.setDisabled(True)
+        self.portEdit.setDisabled(True)
+        self.userEdit.setDisabled(True)
+        self.passwordEdit.setDisabled(True)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    win = MyMainWindow()
+    win.show()
+    sys.exit(app.exec_())
