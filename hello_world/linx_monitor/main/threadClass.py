9a806fe9ad2fc42206aa83a55312bd4dca56bd59
WatsonJay <446725026@qq.com>
2019-06-13 00:28:44 +0800

    终止信号接口

--- hello_world/linx_monitor/main/threadClass.py
+++ hello_world/linx_monitor/main/threadClass.py
@@ -5,11 +5,18 @@ from PyQt5.QtCore import QThread, pyqtSignal
 
 
 class Thread(QThread):
-    getMsgSignal = pyqtSignal(int)
+    getMsgSignal = pyqtSignal(int, int, int)
     def __init__(self):
         super().__init__()
+        self.working = True
 
     def run(self):
-        for i in range(5):
-            self.getMsgSignal.emit(random.randint(1,100))
-            sleep(5)
+        while self.working :
+            self.getMsgSignal.emit(random.randint(1,100), random.randint(1,100), random.randint(1,100))
+            sleep(1)
+
+    def __del__(self):
+        # 线程状态改变与线程终止
+        self.working = False
+        self.wait()
+        self.terminate()
