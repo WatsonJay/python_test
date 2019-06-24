0d003a362d1db0723e418592a8afd63c22ad58ad
WatsonJay <446725026@qq.com>
2019-06-24 08:53:51 +0800

    BUG修复

--- hello_world/linx_monitor/main/main.py
+++ hello_world/linx_monitor/main/main.py
@@ -1021,5 +1021,5 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
     win = password_form()
     WindowShow = MyMainWindow()
-    WindowShow.show()
+    win.show()
     sys.exit(app.exec_())
