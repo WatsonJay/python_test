e17f8b96fc2ae8da081210a48e287a9b03d20891
WatsonJay <446725026@qq.com>
2019-08-31 11:01:56 +0800

    Update 3-蜂鸟网.py

--- hello_world/reptile_training/3-蜂鸟网.py
+++ hello_world/reptile_training/3-蜂鸟网.py
@@ -1,3 +1,6 @@
+import os
+import time
+
 import requests
 #json解析库,对应到lxml
 import json
@@ -11,7 +14,14 @@ headers = {'Host': 'photo.fengniao.com',
             'X-Requested-With':'XMLHttpRequest',
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:65.0) Gecko/20100101 Firefox/65.0'
                           }
-def get_info_url(url):
+def get_info_url(url,page):
+    is_exists = os.path.exists("./fengniao")
+    # 判断结果
+    if not is_exists:
+        # 如果不存在则创建目录
+        # 创建目录操作函数
+        os.makedirs("./fengniao")
+    ip = free_proxyIP.proxyip()
     ip = free_proxyIP.proxyip()
     Proxy_Ip = {'http': ip}
     html = requests.get(url, headers=headers, proxies=Proxy_Ip, timeout=3)
@@ -19,8 +29,18 @@ def get_info_url(url):
     unicodestr = json.loads(html.text)
     # python形式的列表
     pic_list = jsonpath.jsonpath(unicodestr, "$..picUrl")
-    print(1)
+    for i,pic_url in enumerate(pic_list):
+        html = requests.get("https://photo.fengniao.com/"+pic_url, headers=headers, proxies=Proxy_Ip, timeout=3)
+        selector = etree.HTML(html.text)
+        pic = selector.xpath('//a[@class="downPic"]/@href')[0]
+        title = selector.xpath('//h3[@class="title overOneTxt"]/text()')[0]
+        filename = '%s/%s/%s.jpg' % (os.path.abspath('.'), "fengniao", str(page)+"-"+str(i+1)+"-"+title)
+        print(u'开始下载图片:%s/%s' % ("fengniao", str(page)+"-"+str(i+1)+"-"+title))
+        with open(filename, "wb") as jpg:
+            jpg.write(requests.get(pic).content)
+            time.sleep(1)
+
 
 if __name__ == '__main__':
-    for url in urls:
-        get_info_url(url)
+    for i,url in enumerate(urls):
+        get_info_url(url,i+1)
